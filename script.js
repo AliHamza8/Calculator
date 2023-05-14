711266e16e85817428f3cd0594128da372cb69db
Ali Hamza <alihamza022bb@gmail.com>
2023-05-14 10:52:59 +0500

    keypress

--- script.js
+++ script.js
@@ -1,67 +1,73 @@
 let textBox = document.querySelector('.header')
 
-let opPlus = document.querySelector('.btn-plus')
-let opMinus = document.querySelector('.btn-minus')
-let opMul = document.querySelector('.btn-mul')
-let opDiv = document.querySelector('.btn-div')
+textBox.onkeydown = e => {
+    if(e.keyCode == 187)
+    console.log("It's =")
+}
 
-let btnNum0 = document.querySelector('.btn-0')
-let btnNum9 = document.querySelector('.btn-9')
-let btnNum8 = document.querySelector('.btn-8')
-let btnNum7 = document.querySelector('.btn-7')
-let btnNum6 = document.querySelector('.btn-6')
-let btnNum5 = document.querySelector('.btn-5')
-let btnNum4 = document.querySelector('.btn-4')
-let btnNum3 = document.querySelector('.btn-3')
-let btnNum2 = document.querySelector('.btn-2')
-let btnNum1 = document.querySelector('.btn-1')
-const btnDot = document.querySelector('.btn-dot')
+// let opPlus = document.querySelector('.btn-plus')
+// let opMinus = document.querySelector('.btn-minus')
+// let opMul = document.querySelector('.btn-mul')
+// let opDiv = document.querySelector('.btn-div')
 
-const btnResult = document.querySelector('.btn-result')
-const btnClear = document.querySelector('.btn-c')
+// let btnNum0 = document.querySelector('.btn-0')
+// let btnNum9 = document.querySelector('.btn-9')
+// let btnNum8 = document.querySelector('.btn-8')
+// let btnNum7 = document.querySelector('.btn-7')
+// let btnNum6 = document.querySelector('.btn-6')
+// let btnNum5 = document.querySelector('.btn-5')
+// let btnNum4 = document.querySelector('.btn-4')
+// let btnNum3 = document.querySelector('.btn-3')
+// let btnNum2 = document.querySelector('.btn-2')
+// let btnNum1 = document.querySelector('.btn-1')
+// const btnDot = document.querySelector('.btn-dot')
 
-btnClear.addEventListener('click', () => {
-    textBox.value = ''
-})
+// const btnResult = document.querySelector('.btn-result')
+// const btnClear = document.querySelector('.btn-c')
 
-// Numbers behaviours
-btnNum9.addEventListener('click', () => {
-    textBox.value += btnNum9.textContent
-})
-btnNum8.addEventListener('click', () => {
-    textBox.value += btnNum8.textContent
-})
-btnNum7.addEventListener('click', () => {
-    textBox.value += btnNum7.textContent
-})
-btnNum6.addEventListener('click', () => {
-    textBox.value += btnNum6.textContent
-})
-btnNum5.addEventListener('click', () => {
-    textBox.value += btnNum5.textContent
-})
-btnNum4.addEventListener('click', () => {
-    textBox.value += btnNum4.textContent
-})
-btnNum3.addEventListener('click', () => {
-    textBox.value += btnNum3.textContent
-})
-btnNum2.addEventListener('click', () => {
-    textBox.value += btnNum2.textContent
-})
-btnNum1.addEventListener('click', () => {
-    textBox.value += btnNum1.textContent
-})
-btnNum0.addEventListener('click', () => {
-    textBox.value += btnNum0.textContent
-})
-btnDot.addEventListener('click', () => {
-    textBox.value += btnDot.textContent
-})
+// // Clear the numbers in textbox
+// btnClear.addEventListener('click', () => {
+//     textBox.value = ''
+// })
 
-//Operators Functions
-btnResult.addEventListener('click', () => {
-    textBox.onkeydown = e => {
-        console.log(e)
-    }
-})
+// // Numbers behaviours
+// btnNum9.addEventListener('click', () => {
+//     textBox.value += btnNum9.textContent
+// })
+// btnNum8.addEventListener('click', () => {
+//     textBox.value += btnNum8.textContent
+// })
+// btnNum7.addEventListener('click', () => {
+//     textBox.value += btnNum7.textContent
+// })
+// btnNum6.addEventListener('click', () => {
+//     textBox.value += btnNum6.textContent
+// })
+// btnNum5.addEventListener('click', () => {
+//     textBox.value += btnNum5.textContent
+// })
+// btnNum4.addEventListener('click', () => {
+//     textBox.value += btnNum4.textContent
+// })
+// btnNum3.addEventListener('click', () => {
+//     textBox.value += btnNum3.textContent
+// })
+// btnNum2.addEventListener('click', () => {
+//     textBox.value += btnNum2.textContent
+// })
+// btnNum1.addEventListener('click', () => {
+//     textBox.value += btnNum1.textContent
+// })
+// btnNum0.addEventListener('click', () => {
+//     textBox.value += btnNum0.textContent
+// })
+// btnDot.addEventListener('click', () => {
+//     textBox.value += btnDot.textContent
+// })
+
+// let myVal = btn.textContent
+
+// //Operators Functions
+// btnResult.addEventListener('click', () => {
+
+// })
